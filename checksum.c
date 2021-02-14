7af347df721d8f31ec7bb73c7b66c60eced5831f
Gerardo Marioni <gerardomarioni@Gerardos-MacBook-Pro.local>
2021-02-13 21:15:25 -0800

    Fixed errors in print and retval but byte still wont work

---
+++ checksum.c
@@ -0,0 +1,41 @@
+/********************************/
+/* Program Name:                */
+/* Author:                      */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
+
+#define max_int (255)
+#define byte (char)
+
+int main (int argc, char * argv[], char ** envp) {
+
+  int count = 10;
+  int sum = 0;   
+/*   byte checksum; 
+  byte complement; */
+  int retval;
+  int fd;
+  fd = open("/Users/gerardomarioni/Desktop/Spring 2021/COMP 122/checksum.c");
+  byte buffer[8];
+  int *p = &buffer;
+  retval = (int) read(fd, (void *) &buffer, count);
+  printf("%d", retval);
+  /* the following is the prototype for the read system call */
+  /* int read(int fildes, void *buf, size_t nbyte);  */
+
+ /*  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  if (checksum != complement ) {
+    fprintf(stderr, "Error Detected!\n"); 
+    return 1;
+  }
+  return 0;
+} */
+}

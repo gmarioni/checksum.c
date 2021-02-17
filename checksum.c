5530a874abd40bb111e5552f136c623609551f14
Gerardo Marioni <gerardomarioni@Gerardos-MacBook-Pro.local>
2021-02-16 16:43:38 -0800

    Input validation detects input out of bounds. Need to get it to end program

--- checksum.c
+++ checksum.c
@@ -4,7 +4,7 @@
 /* Date:    2-16-2021            */
 /********************************/
 /* Description:  Checksum program written in C */
-/* Validation Checks:           */
+/* Validation Checks:   ensure that input values are within 0...(2^8)-1  */
 /* Enhancements:       none     */
 /********************************/
 
@@ -34,6 +34,10 @@ int main (int argc, char * argv[], char ** envp) {
   /* The loop works, just need to now add input validation for the read
   part just before the loop*/
   read(STDIN_FILENO, &header, count);
+  if(header[0]<0 || header[1]>max_int || header[1]<0 || header[1]<max_int) {
+    fprintf(stdout, "Error, invalid input. Ensure that values are within range of 0..(2^8)-1 \n");
+
+  }
   runningChecksum = (carryForward + header[0] + header[1])%(max_int+1);
   carryForward = (carryForward + header[0] + header[1])/(max_int+1);
     
@@ -43,9 +47,9 @@ int main (int argc, char * argv[], char ** envp) {
         runningChecksum = (carryForward + runningChecksum + 0)%(max_int + 1);
         checksum = header[c];
 
-      } /* else if (header[c] < 0 || header[c] > max_int) {
+      } else if (header[c] < 0 || header[c] > max_int) {
         fprintf(stdout, "Error, invalid input. Ensure that values are within range of 0..(2^8)-1 \n");
-      }  */else {
+      } else {
         carryForward = (runningChecksum + header[c])/(max_int + 1);
         runningChecksum = (carryForward + runningChecksum + header[c])%(max_int + 1);
 
